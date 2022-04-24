ca935f601059a8ce5e8f927f626447c34a96fa53
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-25 05:15:15 +0700

    Create main app for payment service

---
+++ payment-service/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/tengkuroman/microshop/payment-service/config"
+	"github.com/tengkuroman/microshop/payment-service/controllers"
+)
+
+func main() {
+	// Connect database
+	db := config.ConnectDatabase()
+	databaseSQL, _ := db.DB()
+	defer databaseSQL.Close()
+
+	// Router
+	r := gin.Default()
+
+	// Set allow CORS
+	r.Use(cors.Default())
+
+	// Set context
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	// Routes (client)
+	r.GET("/payment", controllers.GetPaymentProviders)
+	r.POST("/payment", controllers.PostPaymentProvider)
+	r.PATCH("/payment/:payment_provider_id", controllers.UpdatePaymentProvider)
+	r.DELETE("/payment/:payment_provider_id", controllers.DeletePaymentProvider)
+
+	// Routes (service)
+	r.POST("/payment/process", controllers.ProcessPayment)
+
+	// Run router
+	r.Run()
+}
