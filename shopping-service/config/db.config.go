b661a0c839edafdead9f93a30994d76178dbd1b1
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-25 06:52:42 +0700

    Create db config for shopping service

---
+++ shopping-service/config/db.config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tengkuroman/microshop/shopping-service/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase() *gorm.DB {
+	username := os.Getenv("SHOPPING_DB_USERNAME")
+	password := os.Getenv("SHOPPING_DB_PASSWORD")
+	host := os.Getenv("SHOPPING_DB_HOST")
+	port := os.Getenv("SHOPPING_DB_PORT")
+	database := os.Getenv("SHOPPING_DB_NAME")
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable", host, username, password, database, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(
+		&models.CartItem{},
+		&models.ShoppingSession{},
+	)
+
+	return db
+}
