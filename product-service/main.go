b9d1dca25c04209f6bdffeef1e8bc99de1ad8921
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-25 06:21:27 +0700

    Implement routes for product services

---
+++ product-service/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/tengkuroman/microshop/product-service/config"
+	"github.com/tengkuroman/microshop/product-service/controllers"
+)
+
+func main() {
+	// Connect database
+	db := config.ConnectDatabase()
+	databaseSQL, _ := db.DB()
+	defer databaseSQL.Close()
+
+	// Router
+	r := gin.Default()
+
+	// Set allow CORS
+	r.Use(cors.Default())
+
+	// Set context
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	// All user
+	r.GET("/products", controllers.GetAllProducts)
+	r.GET("/product/:product_id", controllers.GetProductByID)
+	r.GET("/products/seller/:user_id", controllers.GetProductsBySellerID)
+	r.GET("/products/category/:category_id", controllers.GetProductsByCategoryID)
+
+	r.GET("/categories", controllers.GetAllCategories)
+	r.GET("/category/:category_id", controllers.GetCategoryByID)
+
+	// Seller
+	r.POST("/product", controllers.PostProduct)
+	r.PATCH("/product/:product_id", controllers.UpdateProduct)
+	r.DELETE("/product/:product_id", controllers.DeleteProduct)
+
+	// Admin
+	r.POST("/category", controllers.PostCategory)
+	r.PATCH("/category/:category_id", controllers.UpdateCategory)
+	r.DELETE("/category/:category_id", controllers.DeleteCategory)
+
+	// Run router
+	r.Run()
+}
