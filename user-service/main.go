77de855cd12bc0e25edc952c7dce76df2c4be03d
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-26 06:27:50 +0700

    Implement routes for user srv

---
+++ user-service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/tengkuroman/microshop/user-service/config"
+	"github.com/tengkuroman/microshop/user-service/controllers"
+)
+
+func main() {
+	// Connect database
+	db := config.ConnectDatabase()
+	databaseSQL, _ := db.DB()
+	defer databaseSQL.Close()
+
+	// Router
+	r := gin.Default()
+
+	// Set allow CORS
+	r.Use(cors.Default())
+
+	// Set context
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	// Routes (public)
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
+
+	// Routes (registered)
+	r.PATCH("/change", controllers.ChangeUserDetail)
+	r.PATCH("/change/password/", controllers.ChangePassword)
+	r.PATCH("/switch/:role", controllers.SwitchUser)
+
+	// Routes (API gateway)
+	r.POST("/auth/validate", controllers.ValidateUser)
+
+	// Run router
+	r.Run()
+}
