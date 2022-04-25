661d100b29e79885c9a3a4006ce15df671e8c6c5
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-26 02:41:22 +0700

    Setup routes for shopping service

---
+++ shopping-service/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/tengkuroman/microshop/shopping-service/config"
+	"github.com/tengkuroman/microshop/shopping-service/controllers"
+)
+
+func main() {
+	// Connect database
+	db := config.ConnectDatabase()
+	databaseSQL, _ := db.DB()
+	defer databaseSQL.Close()
+
+	// Router
+	r := gin.Default()
+
+	// Set allow CORS
+	r.Use(cors.Default())
+
+	// Set context
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	// Buyer route
+	r.POST("/cart", controllers.AddProductToCart)
+	r.GET("/cart", controllers.GetCartItems)
+	r.PATCH("/cart", controllers.UpdateCartItem)
+	r.DELETE("/cart", controllers.DropCart)
+	r.GET("/cart/checkout", controllers.Checkout)
+
+	// Run router
+	r.Run()
+}
