b120b738e2c43b75ca0248f6f405072ba05c5df9
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-26 10:45:52 +0700

    Revise model to accomodate response structure

--- shopping-service/models/product.model.go
+++ shopping-service/models/product.model.go
@@ -1,10 +1,12 @@
 package models
 
-import "gorm.io/gorm"
-
-type Product struct {
-	gorm.Model
-	Name, Description, ImageURL string
-	Price                       int
-	UserID, CategoryID          uint
+type ProductResponse struct {
+	Data struct {
+		Name        string `binding:"required"`
+		Description string `binding:"required"`
+		ImageURL    string `json:"image_url" binding:"required"`
+		Price       int    `binding:"required"`
+		UserID      uint   `json:"user_id" binding:"required"`
+		CategoryID  uint   `json:"category_url" binding:"required"`
+	} `binding:"required"`
 }
