1e21fd476d186935765f32fcf473e32497d359d4
Tengku Romansyah <tengku.romansyah@gmail.com>
2022-04-30 07:09:55 +0700

    Add comment

--- etc/custom-auth.lua
+++ etc/custom-auth.lua
@@ -4,11 +4,12 @@ local cjson = require "cjson"
 local access_token = kong.request.get_header("Authorization")
 
 if not access_token then
-    kong.response.exit(401)  --unauthorized
+    kong.response.exit(401)  -- Unauthorized
 end
 
 local httpc = http:new()
 
+-- Check user validation and get user info
 local res, err = httpc:request_uri("http://user-srv:8082/auth/validate", {
     method = "POST",
     ssl_verify = false,
@@ -18,11 +19,12 @@ local res, err = httpc:request_uri("http://user-srv:8082/auth/validate", {
 })
 
 if not res then
-    kong.response.exit(500)  --internal error
+    kong.response.exit(500)  -- Internal error
 end
 
 user_info = cjson.decode(res.body)
 
+-- Delete bearer token and set user info to request header
 kong.service.request.clear_header("Authorization")
 kong.service.request.set_header("X-User-ID", user_info.user_id)
 kong.service.request.set_header("X-User-Role", user_info.role)
